0c2dff4256032d1700f5d4c7ccf2513c83cffde0
ziw1 <111111@qq.com>
2018-07-12 15:02:45 +0800

    声音流畅，去掉fa&si

--- pages/piano/piano.js
+++ pages/piano/piano.js
@@ -5,13 +5,18 @@ Page({
    * 页面的初始数据
    */
   data: {
-    testRes: '点击下一题开始。',
+    testRes: '点击下一题开始。'
   },
 
 //下一题
   doTest: function () {
     
     var rn = Math.floor(Math.random() * 8 + 1);
+     //不听 fa 和 si
+    while (rn == 4 || rn == 7){
+      rn = Math.floor(Math.random() * 8 + 1);
+    }
+
     //console.log(rn);
     this.testDegree = rn;
     // this.setData({
@@ -20,87 +25,69 @@ Page({
     this.palyByNum(rn);
     
   },
-
+ 
   palyByNum : function(num){
-    this.stopAll();
+    var voice = wx.createInnerAudioContext();
     switch (num) {
       case 1:
-        this.C4.play()
+        voice.src = 'pages/piano/source/40-C.mp3';
         break;
       case 2:
-        this.D4.play()
+        voice.src = 'pages/piano/source/42-D.mp3';
         break;
       case 3:
-        this.E4.play()
+        voice.src = 'pages/piano/source/44-E.mp3';
         break;
       case 4:
-        this.F4.play()
+        voice.src = 'pages/piano/source/45-F.mp3';
         break;
       case 5:
-        this.G4.play()
+        voice.src = 'pages/piano/source/47-G.mp3';
         break;
       case 6:
-        this.A4.play()
+        voice.src = 'pages/piano/source/49-A.mp3';
         break;
       case 7:
-        this.B4.play()
+        voice.src = 'pages/piano/source/51-B.mp3';
         break;
       case 8:
-        this.C5.play()
+        voice.src = 'pages/piano/source/52-C.mp3';
         break;
     }
+    voice.play();
   },
 
   playC4: function () {
-    this.stopAll();
-    this.C4.play();
+    this.palyByNum(1);
     this.checkTest(1);
   },
   playD4: function () {
-    this.stopAll();
-    this.D4.play();
+    this.palyByNum(2);
     this.checkTest(2);
   },
   playE4: function () {
-    this.stopAll();
-    this.E4.play();
+    this.palyByNum(3);
     this.checkTest(3);
   },
   playF4: function () {
-    this.stopAll();
-    this.F4.play();
+    this.palyByNum(4);
     this.checkTest(4);
   },
   playG4: function () {
-    this.stopAll();
-    this.G4.play();
+    this.palyByNum(5);
     this.checkTest(5);
   },
   playA4: function () {
-    this.stopAll();
-    this.A4.play();
+    this.palyByNum(6);
     this.checkTest(6);
   },
   playB4: function () {
-    this.stopAll();
-    this.B4.play();
+    this.palyByNum(7);
     this.checkTest(7);
   },
   playC5: function () {
-    this.stopAll();
-    this.C5.play();
+    this.palyByNum(8);
     this.checkTest(8);
-  },
-
-  stopAll : function(){
-    this.C4.stop();
-    this.D4.stop();
-    this.E4.stop();
-    this.F4.stop();
-    this.G4.stop();
-    this.A4.stop();
-    this.B4.stop();
-    this.C5.stop();
   },
 //检测答题是否正确
   checkTest : function(touchNum){
@@ -124,6 +111,7 @@ Page({
   },
 //再听一遍
   doAgain : function(){
+
     if (this.testDegree == undefined) {
       wx.showToast({
         title: '请点击下一题' ,
@@ -135,11 +123,7 @@ Page({
     this.palyByNum(this.testDegree);
   },
 
-
-
-
-
-
+  
   /**
    * 生命周期函数--监听页面加载
    */
@@ -151,32 +135,6 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    
-    this.C4 = wx.createInnerAudioContext()
-    this.C4.src = 'http://ziwen.imwork.net:19791/music-source/40-C.mp3'
-
-    this.D4 = wx.createInnerAudioContext()
-    this.D4.src = 'http://ziwen.imwork.net:19791/music-source/42-D.mp3'
-
-    this.E4 = wx.createInnerAudioContext()
-    this.E4.src = 'http://ziwen.imwork.net:19791/music-source/44-E.mp3'
-
-    this.F4 = wx.createInnerAudioContext()
-    this.F4.src = 'http://ziwen.imwork.net:19791/music-source/45-F.mp3'
-
-    this.G4 = wx.createInnerAudioContext()
-    this.G4.src = 'http://ziwen.imwork.net:19791/music-source/47-G.mp3'
-
-    this.A4 = wx.createInnerAudioContext()
-    this.A4.src = 'http://ziwen.imwork.net:19791/music-source/49-A.mp3'
-
-    this.B4 = wx.createInnerAudioContext() 
-    this.B4.src = 'http://ziwen.imwork.net:19791/music-source/51-B.mp3'
-
-    this.C5 = wx.createInnerAudioContext()
-    this.C5.src = 'http://ziwen.imwork.net:19791/music-source/52-C.mp3'
-    
-   
   },
 
   /**
